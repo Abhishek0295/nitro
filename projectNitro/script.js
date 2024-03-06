d9bf71e7e76510520fe839cafebf4b31958154d5
Abhishek <150647082+Abhishek0295@users.noreply.github.com>
2024-03-06 18:04:20 +0530

    Add files via upload

---
+++ projectNitro/script.js
@@ -0,0 +1,28 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true 
+      },
+      dynamicBullets: true,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+
+// Animation
+  AOS.init();
+
